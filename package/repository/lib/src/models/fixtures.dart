b5bb719f86b3f7b37886fc8aaa2258115406b0d7
Mateocaicedo <caicedomateo9@gmail.com>
2022-11-07 22:26:30 -0500

    task: some code

--- package/repository/lib/src/models/fixtures.dart
+++ package/repository/lib/src/models/fixtures.dart
@@ -5,7 +5,6 @@ class Fixtures {
     Fixtures({
         required this.eventKey,
         required this.eventDate,
-        required this.eventTime,
         required this.eventHomeTeam,
         required this.homeTeamKey,
         required this.eventAwayTeam,
@@ -17,20 +16,14 @@ class Fixtures {
         required this.countryName,
         required this.leagueName,
         required this.leagueKey,
-        required this.leagueRound,
-        required this.leagueSeason,
         required this.eventStadium,
-        required this.eventReferee,
         required this.homeTeamLogo,
         required this.awayTeamLogo,
-        required this.eventCountryKey,
         required this.leagueLogo,
-        required this.countryLogo,
     });
 
     String eventKey;
     DateTime eventDate;
-    String eventTime;
     String eventHomeTeam;
     String homeTeamKey;
     String eventAwayTeam;
@@ -42,20 +35,13 @@ class Fixtures {
     String countryName;
     String leagueName;
     String leagueKey;
-    String leagueRound;
-    String leagueSeason;
     String eventStadium;
-    String eventReferee;
     String homeTeamLogo;
     String awayTeamLogo;
-    String eventCountryKey;
     String leagueLogo;
-    String countryLogo;
-
     factory Fixtures.fromJson(Map<String, dynamic> json) => Fixtures(
         eventKey: json["event_key"].toString(),
         eventDate: DateTime.parse(json["event_date"].toString()),
-        eventTime: json["event_time"].toString(),
         eventHomeTeam: json["event_home_team"].toString(),
         homeTeamKey: json["home_team_key"].toString(),
         eventAwayTeam: json["event_away_team"].toString(),
@@ -67,21 +53,15 @@ class Fixtures {
         countryName: json["country_name"].toString(),
         leagueName: json["league_name"].toString(),
         leagueKey: json["league_key"].toString(),
-        leagueRound: json["league_round"].toString(),
-        leagueSeason: json["league_season"].toString(),
         eventStadium: json["event_stadium"].toString(),
-        eventReferee: json["event_referee"].toString(),
         homeTeamLogo: json["home_team_logo"].toString(),
         awayTeamLogo: json["away_team_logo"].toString(),
-        eventCountryKey: json["event_country_key"].toString(),
         leagueLogo: json["league_logo"].toString(),
-        countryLogo: json["country_logo"].toString(),
     );
 
     Map<String, dynamic> toJson() => {
         "event_key": eventKey,
         "event_date": "${eventDate.year.toString().padLeft(4, '0')}-${eventDate.month.toString().padLeft(2, '0')}-${eventDate.day.toString().padLeft(2, '0')}",
-        "event_time": eventTime,
         "event_home_team": eventHomeTeam,
         "home_team_key": homeTeamKey,
         "event_away_team": eventAwayTeam,
@@ -93,14 +73,9 @@ class Fixtures {
         "country_name": countryName,
         "league_name": leagueName,
         "league_key": leagueKey,
-        "league_round": leagueRound,
-        "league_season": leagueSeason,
         "event_stadium": eventStadium,
-        "event_referee": eventReferee,
         "home_team_logo": homeTeamLogo,
         "away_team_logo": awayTeamLogo,
-        "event_country_key": eventCountryKey,
         "league_logo": leagueLogo,
-        "country_logo": countryLogo,
     };
 }
--- package/repository/lib/src/models/player.dart
+++ package/repository/lib/src/models/player.dart
@@ -18,13 +18,7 @@ class Player {
        required this.playerAge,
        required this.playerMatchPlayed,
        required this.playerGoals,
-       required this.playerYellowCards,
-       required this.playerRedCards,
-       required this.playerMinutes,
        required this.playerAssists,
-       required this.playerIsCaptain,
-       required this.playerShotsTotal,
-       required this.playerGoalsConceded,
        required this.playerRating,
        required this.teamName,
        required this.teamKey,
@@ -39,13 +33,7 @@ class Player {
     String playerAge;
     String playerMatchPlayed;
     String playerGoals;
-    String playerYellowCards;
-    String playerRedCards;
-    String playerMinutes;
     String playerAssists;
-    String playerIsCaptain;
-    String playerShotsTotal;
-    String playerGoalsConceded;
     String playerRating;
     String teamName;
     String teamKey;
@@ -60,13 +48,7 @@ class Player {
         playerAge: json["player_age"].toString(),
         playerMatchPlayed: json["player_match_played"].toString(),
         playerGoals: json["player_goals"].toString(),
-        playerYellowCards: json["player_yellow_cards"].toString(),
-        playerRedCards: json["player_red_cards"].toString(),
-        playerMinutes: json["player_minutes"].toString(),
         playerAssists: json["player_assists"].toString(),
-        playerIsCaptain: json["player_is_captain"].toString(),
-        playerShotsTotal: json["player_shots_total"].toString(),
-        playerGoalsConceded: json["player_goals_conceded"].toString(),
         playerRating: json["player_rating"].toString(),
         teamName: json["team_name"].toString(),
         teamKey: json["team_key"].toString(),
@@ -82,13 +64,7 @@ class Player {
         "player_age": playerAge,
         "player_match_played": playerMatchPlayed,
         "player_goals": playerGoals,
-        "player_yellow_cards": playerYellowCards,
-        "player_red_cards": playerRedCards,
-        "player_minutes": playerMinutes,
         "player_assists": playerAssists,
-        "player_is_captain": playerIsCaptain,
-        "player_shots_total": playerShotsTotal,
-        "player_goals_conceded": playerGoalsConceded,
         "player_rating": playerRating,
         "team_name": teamName,
         "team_key": teamKey,
